8117df4c10371f06ed2800acfda16e4394a47d32
chayut005 <64309010005@chontech.ac.th>
2021-12-02 16:37:57 +0700

    Hadead

--- showdemo.php
+++ showdemo.php
@@ -10,7 +10,7 @@ $dbname = "it64309010005";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_chatset($conn,"utf8");
+mysqli_set_charset($conn,"utf8");
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
